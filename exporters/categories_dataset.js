4c7bc2860b61bd101be01a1cc5e66c8375579813
Róbert Viðar Bjarnason <robert@citizens.is>
2016-03-01 20:44:53 +0000

    Remove linefeeds

--- exporters/categories_dataset.js
+++ exporters/categories_dataset.js
@@ -66,6 +66,7 @@ async.series([
           }
           content = content.replace(',',' ');
           content = content.replace('\n',' ');
+          content = content.replace('"',' ');
           content = content.replace(/(\r\n|\n|\r)/gm," ");
           categories[post.category_id].push(content);
           seriesCallback();
