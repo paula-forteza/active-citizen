31c40254258abc5b3eddd4127864636ac2d22b46
Róbert Viðar Bjarnason <robert@citizens.is>
2016-03-20 11:06:00 +0000

    Added news feeds for groups and communities

--- controllers/news_feeds.js
+++ controllers/news_feeds.js
@@ -41,4 +41,67 @@ router.get('/domains/:id', auth.can('view domain'), auth.isLoggedIn, function(re
   });
 });
 
+router.get('/groups/:id', auth.can('view group'), auth.isLoggedIn, function(req, res) {
+
+  var options = {
+    user_id: req.user.id,
+    group_id: req.params.id
+  };
+
+  if (req.query.afterDate) {
+    options = _.merge(options, {
+      afterDate: new Date(req.query.afterDate)
+    })
+  }
+
+  if (req.query.beforeDate) {
+    options = _.merge(options, {
+      beforeDate: new Date(req.query.beforeDate)
+    })
+  }
+
+  getCuratedNewsItems(options, function (error, activities, oldestProcessedActivityAt) {
+    if (error) {
+      log.error("News Feed Error Domain", { groupId: req.params.id, userId: req.user.id, errorStatus:  500 });
+      res.sendStatus(500);
+    } else {
+      res.send({
+        activities: activities,
+        oldestProcessedActivityAt: oldestProcessedActivityAt
+      });
+    }
+  });
+});
+
+router.get('/communities/:id', auth.can('view community'), auth.isLoggedIn, function(req, res) {
+
+  var options = {
+    user_id: req.user.id,
+    community_id: req.params.id
+  };
+
+  if (req.query.afterDate) {
+    options = _.merge(options, {
+      afterDate: new Date(req.query.afterDate)
+    })
+  }
+
+  if (req.query.beforeDate) {
+    options = _.merge(options, {
+      beforeDate: new Date(req.query.beforeDate)
+    })
+  }
+
+  getCuratedNewsItems(options, function (error, activities, oldestProcessedActivityAt) {
+    if (error) {
+      log.error("News Feed Error Domain", { communityId: req.params.id, userId: req.user.id, errorStatus:  500 });
+      res.sendStatus(500);
+    } else {
+      res.send({
+        activities: activities,
+        oldestProcessedActivityAt: oldestProcessedActivityAt
+      });
+    }
+  });
+});
 module.exports = router;
