2f592f4cb64551e9231d6fc426604fcbc7b15dbf
Róbert Viðar Bjarnason <robert@citizens.is>
2016-05-25 21:49:07 +0000

    Better subjects for notifications emails

--- engine/notifications/point_delivery.js
+++ engine/notifications/point_delivery.js
@@ -4,9 +4,10 @@ var i18n = require('../../utils/i18n');
 var filterNotificationForDelivery = require('./emails_utils').filterNotificationForDelivery;
 
 module.exports = function (notification, user, callback) {
+  var post = notification.AcActivities[0].Post;
   if (notification.type=='notification.point.new') {
-    filterNotificationForDelivery(notification, user, 'point_activity', i18n.t('notification.email.newPointOnMyPoint'), callback);
+    filterNotificationForDelivery(notification, user, 'point_activity', i18n.t('notification.email.newPointOnMyPoint')+": "+post.name, callback);
   } else if (notification.type=='notification.point.quality') {
-    filterNotificationForDelivery(notification, user, 'point_activity', i18n.t('email.point.subject.quality'), callback);
+    filterNotificationForDelivery(notification, user, 'point_activity', i18n.t('notification.email.newPointQuality')+": "+post.name, callback);
   }
 };
--- engine/notifications/post_delivery.js
+++ engine/notifications/post_delivery.js
@@ -4,10 +4,11 @@ var i18n = require('../../utils/i18n');
 var filterNotificationForDelivery = require('./emails_utils').filterNotificationForDelivery;
 
 module.exports = function (notification, user, callback) {
+  var post = notification.AcActivities[0].Post;
   if (notification.type=='notification.post.new') {
-    filterNotificationForDelivery(notification, user, 'post_activity', i18n.t('notification.email.postNew'), callback);
+    filterNotificationForDelivery(notification, user, 'post_activity', i18n.t('notification.email.postNew')+": "+post.name, callback);
   } else if (notification.type=='notification.post.endorsement') {
-    filterNotificationForDelivery(notification, user, 'post_activity', i18n.t('notification.email.postNew'), callback);
+    filterNotificationForDelivery(notification, user, 'post_activity', i18n.t('notification.email.postEndorsementNew')+": "+post.name, callback);
   } else {
     callback();
   }
