ef44205871802577da2f16821f4b6bc3cebe5cf5
Robert Bjarnason <Robert Bjarnason>
2017-01-06 18:37:17 +0000

    Refactor sending email function

--- engine/notifications/emails_utils.js
+++ engine/notifications/emails_utils.js
@@ -3,6 +3,40 @@ var log = require('../../utils/logger');
 var toJson = require('../../utils/to_json');
 var async = require('async');
 var queue = require('../../workers/queue');
+var path = require('path');
+var EmailTemplate = require('email-templates').EmailTemplate;
+var nodemailer = require('nodemailer');
+var ejs = require('ejs');
+var i18n = require('../utils/i18n');
+var airbrake = require('../utils/airbrake');
+
+var templatesDir = path.resolve(__dirname, '..', 'email_templates', 'notifications');
+
+var DEBUG_EMAILS_TO_TEMP_FIlE = false;
+
+var i18nFilter = function(text) {
+  return i18n.t(text);
+};
+
+var transport = nodemailer.createTransport({
+  service: 'sendgrid',
+  auth: {
+    user: process.env.SENDGRID_USERNAME,
+    pass: process.env.SENDGRID_PASSWORD
+  }
+});
+
+var translateSubject = function (subjectHash) {
+  var subject = i18n.t(subjectHash.translateToken);
+  if (subjectHash.contentName) {
+    subject += ": "+subjectHash.contentName
+  }
+  return subject;
+};
+
+var linkTo = function (url) {
+  return '<a href="'+url+'">'+url+'</a>';
+};
 
 var filterNotificationForDelivery = function (notification, user, template, subject, callback) {
   var method = user.notifications_settings[notification.from_notification_setting].method;
@@ -68,6 +102,124 @@ var filterNotificationForDelivery = function (notification, user, template, subj
   }
 };
 
+var sendOneEmail = function (emailLocals, callback) {
+  var template, fromEmail;
+
+  async.series([
+    function (seriesCallback) {
+      if (emailLocals.domain && emailLocals.domain.domain_name) {
+        seriesCallback();
+      } else {
+        log.error("EmailWorker Can't find domain for email", {emailLocals: emailLocals});
+        seriesCallback("Can't find domain for email");
+      }
+    },
+
+    function (seriesCallback) {
+      log.info("EmailWorker Started Setup", {});
+
+      template = new EmailTemplate(path.join(templatesDir, emailLocals.template));
+
+      emailLocals['t'] = i18nFilter;
+      emailLocals['linkTo'] = linkTo;
+
+      if (!emailLocals['community']) {
+        emailLocals['community'] = {hostname: 'www'}
+      }
+
+      if (emailLocals.domain.domain_name.indexOf('betrireykjavik.is') > -1) {
+        fromEmail = 'betrireykjavik@ibuar.is';
+      } else if (emailLocals.domain.domain_name.indexOf('betraisland.is') > -1) {
+        fromEmail = 'betraisland@ibuar.is';
+      } else {
+        fromEmail = "admin@yrpri.org";
+      }
+
+      emailLocals.headerImageUrl = "";
+      seriesCallback();
+    },
+
+    function (seriesCallback) {
+      log.info("EmailWorker Started Locale", {});
+      var locale;
+
+      if (emailLocals.user.default_locale && emailLocals.user.default_locale != "") {
+        locale = emailLocals.user.default_locale;
+      } else if (emailLocals.community && emailLocals.community.default_locale && emailLocals.community.default_locale != "") {
+        locale = emailLocals.community.default_locale;
+      } else if (emailLocals.domain && emailLocals.domain.default_locale && emailLocals.domain.default_locale != "") {
+        locale = emailLocals.domain.default_locale;
+      } else {
+        locale = 'en';
+      }
+
+      log.info("EmailWorker Selected locale", {locale: locale});
+
+      i18n.changeLanguage(locale, function (err, t) {
+        seriesCallback(err);
+      });
+    },
+
+    function (seriesCallback) {
+      log.info("EmailWorker Started Sending", {});
+
+      template.render(emailLocals, function (error, results) {
+        if (error) {
+          log.error('EmailWorker Error', { err: error, userId: emailLocals.user.id });
+          seriesCallback(error);
+        } else {
+          var translatedSubject = translateSubject(emailLocals.subject);
+
+          if (process.env.SENDGRID_USERNAME) {
+            transport.sendMail({
+              from: fromEmail, // emailLocals.community.admin_email,
+              to: emailLocals.user.email,
+              bcc: 'robert@citizens.is',
+              subject: translatedSubject,
+              html: results.html,
+              text: results.text
+            }, function (error, responseStatus) {
+              if (error) {
+                log.error('EmailWorker', { err: error, user: emailLocals.user });
+                seriesCallback(error);
+              } else {
+                log.info('EmailWorker Completed', { responseStatusMessage: responseStatus.message, email: emailLocals.user.email, userId: emailLocals.user.id });
+                seriesCallback();
+              }
+            })
+          } else {
+            log.warn('EmailWorker no SMTP server', { subject: translatedSubject, userId: emailLocals.user.id, resultsHtml: results.html , resultsText: results.text });
+            if (DEBUG_EMAILS_TO_TEMP_FIlE) {
+              var fs = require('fs');
+              fs.writeFile("/tmp/testHtml.html", results.html, function(err) {
+                if(err) {
+                  console.log(err);
+                }
+                seriesCallback();
+              });
+            } else {
+              seriesCallback();
+            }
+          }
+        }
+      });
+    }
+  ], function (error) {
+    if (error) {
+      log.error("EmailWorker Error", {err: error});
+      airbrake.notify(error, function(airbrakeErr, url) {
+        if (airbrakeErr) {
+          log.error("AirBrake Error", { context: 'airbrake', err: airbrakeErr, errorStatus: 500 });
+        }
+        callback(error);
+      });
+    } else {
+      callback();
+    }
+  });
+};
+
 module.exports = {
-  filterNotificationForDelivery: filterNotificationForDelivery
+  filterNotificationForDelivery: filterNotificationForDelivery,
+  sendOneEmail: sendOneEmail
 };
