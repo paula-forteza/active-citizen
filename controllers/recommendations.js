2a5bd2bcf8e84127083134a00ee5505ad243a345
Robert Bjarnason <Robert Bjarnason>
2017-01-08 20:06:28 +0000

    Always send 200 from recommendations even if there is an error

--- controllers/recommendations.js
+++ controllers/recommendations.js
@@ -103,6 +103,11 @@ var processRecommendations = function (levelType, req, res, recommendedItemIds, 
     res.send(posts);
   }).catch(function(error) {
     log.error("Recommendation Error "+levelType, { err: error, id: req.params.id, userId:  req.user ? req.user.id : -1, errorStatus: 500 });
+    airbrake.notify(error, function(airbrakeErr, url) {
+      if (airbrakeErr) {
+        log.error("AirBrake Error", { context: 'airbrake', err: airbrakeErr, errorStatus: 500 });
+      }
+    });
     res.sendStatus(200);
   });
 };
