23f481e3a92ce8cdd8f980bff6ddcaf828efa574
Emmanuel Raviart <emmanuel@raviart.com>
2017-12-22 15:21:15 +0100

    Add french localization.

--- workers/main.js
+++ workers/main.js
@@ -16,7 +16,7 @@ var localesPath = path.resolve(__dirname, '../locales');
 i18n
   .use(Backend)
   .init({
-    preload: ['en','is','hr','pl','no','nl','hu','zh_TW'],
+    preload: ['en','is','hr','pl','no','nl','hu','zh_TW', 'fr'],
 
     fallbackLng:'en',
 
